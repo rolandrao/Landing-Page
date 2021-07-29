e7e5f745baa6150197c510ef399e429d8cc5f89b
rolandrao <rolandrao@gmail.com>
2021-07-29 18:56:38 -0400

    adding new code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+var name = "Roland Rao";
+var email = "rolandrao";
+var company = "Nuvalence";
+var product = "";
+var customerid = "";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onLoad={
+        resolveCustomer()
+    }>
+    <div class="container">
+        <dl class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="row">
+                        <dt class="col-sm-2">
+                          <label for="full-name">Full Name</label>
+                        </dt>
+                        <dd class="col-sm-10">
+                            <label class="input-text" id="full-name">{name}</label>
+                        </dd>
+                    </div>
+                    <div class="row">
+                        <dt class="col-sm-2">
+                            <label for="beneficiary-email">Beneficiary Email</label>
+                        </dt>
+                        <dd class="col-sm-10">
+                            <label class = "input-text" id="beneficiary-email">{email}</label>
+
+                        </dd>
+                    </div>
+                    <div class="row">
+                        <dt class="col-sm-2">
+                            <label for="company-name">Company Name</label>
+                        </dt>
+                        <dd class="col-sm-10">
+                            <label class="input-text" id="company-name">{company}</label> 
+                        </dd>
+                    </div>
+                    <div class="row">
+                        <dt class="col-sm-2">
+                            <label for="customer-id">Customer ID</label>
+                        </dt>
+                        <dd class="col-sm-10">
+                            <label class="input-text" id="customer-id">{customerid}</label>
+                        </dd>
+                    </div>
+                    <div class="row">
+                        <dt class="col-sm-2">
+                            <label for="product-code">Product Code</label>
+                        </dt>
+                        <dd class="col-sm-10">
+                            <label class="input-text" id="product-code">{product}</label>
+                        </dd>
+                    </div>
+                </div>
+
+
+            </div>
+        </dl>
     </div>
+    <div>
+        <input type="submit" value="Submit" class="btn form-button" />
+    </div>
+</form>
   );
+}
+
+function resolveCustomer(){
+    var customer = {
+        CustomerIdentifier: "1234",
+        ProductCode: "Test-Product"
+    }
+
+    product = customer.ProductCode;
+
+    customerid = customer.CustomerIdentifier;
+
+
 }
 
 export default App;
